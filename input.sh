cb770681a750e73dabbcf59027708766beec3316
SrinathDsn <srinathsree1133@gmail.com>
2022-04-20 22:47:55 -0400

    Input special variables

--- input.sh
+++ input.sh
@@ -2,3 +2,12 @@
 
 read -p 'Enter your Name: ' name
 echo "Your name = $name"
+
+
+#Special Variables
+#$0-$n , $* / $@, $#
+
+echo Script Name = $0
+echo First Argument = $1
+echo All Arguments = $*
+echo Number of Arguments = $#
