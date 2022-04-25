d56a92f54c38f84dc3103dba41026e76128626a8
SrinathDsn <srinathsree1133@gmail.com>
2022-04-25 15:37:51 -0400

    mysql script

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -15,20 +15,11 @@ Print "Start MySQL Service"
 systemctl enable mysqld &>>${LOG_FILE} && systemctl start mysqld &>>${LOG_FILE}
 StatCheck $?
 
-echo 'show databases' | mysql -uroot -pRoboShop@1 &>>${LOG_FILE}
-if [ $? -ne 0 ]; then
-  Print "Change Default root password"
-  echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('RoboShop@1');" >/tmp/rootpass.sql
-  DEFAULT_ROOT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
-  mysql --connect-expired-password -uroot -p"${DEFAULT_ROOT_PASSWORD}" </tmp/rootpass.sql &>>${LOG_FILE}
-  StatCheck $?
-fi
+echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('RoboShop@1');" >/tmp/rootpass.sql
 
-echo show plugins | mysql -uroot -pRoboShop@1 2>>${LOG_FILE} | grep validate_password
-if [ $? -eq 0 ]; then
-  echo "Uninstall Password Validate Plugin"
-  echo 'uninstall plugin validate_password;' >/tmp/pass-validate.sql
-  mysql --connect-expired-password -uroot -pRoboShop@1 </tmp/pass-validate.sql &>>${LOG_FILE}
+DEFAULT_ROOT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
+mysql -uroot -p"${DEFAULT_ROOT_PASSWORD}" </tmp/rootpass.sql &>>${LOG_FILE}
   StatCheck $?
-fi
+
+
 
