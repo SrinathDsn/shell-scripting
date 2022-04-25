0837bdf2eebdca78660f7273f20be85f88253f72
SrinathDsn <srinathsree1133@gmail.com>
2022-04-24 22:16:52 -0400

    Functions

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -9,21 +9,25 @@ StatCheck() {
   fi
 }
 
+Print() {
+  echo -e "\e[36m $11 \e[0m"
+}
+
 USER_ID=$(id -u)
 if [ "$USER_ID" -ne 0]; then
   echo You should run your script as sudo or root user
   exit 1
 fi
 
-echo -e "\e[36m Installing Nginx \e[0m"
+Print "Installing Nginx"
 yum install nginx -y
 StatCheck $?
 
-echo -e "\e[36m Downloading Nginx Content \e[0m"
+Print "Downloading Nginx Content"
 curl -f -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
 StatCheck $?
 
-echo -e "\e[36m Cleanup Old Nginx content and extract new downloaded archive \e[0m"
+Print "Cleanup Old Nginx content and extract new downloaded archive"
 rm -rf /usr/share/nginx/html/*
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
@@ -33,7 +37,7 @@ rm -rf frontend-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 StatCheck $?
 
-echo -e "\e[36m Starting Nginx \e[0m"
+Print "Starting Nginx"
 systemctl restart nginx
 StatCheck $?
 systemctl enable nginx
