0125be06e547d1f651fde52978ea54ed2bce9e55
SrinathDsn <srinathsree1133@gmail.com>
2022-04-25 17:10:25 -0400

    RabbitMQ Script

--- roboshop/components/rabbitmq.sh
+++ roboshop/components/rabbitmq.sh
@@ -22,6 +22,6 @@ if [ $? -ne 0 ]; then
 fi
 
 Print "Configure Application User"
-rabbitmqctl set_user_tags roboshop administrator && rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+rabbitmqctl set_user_tags roboshop administrator &>>${LOG_FILE} && rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>${LOG_FILE}
 StatCheck $?
 
