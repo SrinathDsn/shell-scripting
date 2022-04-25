3b945de4e68f6f01ac5ddb73b1d38c72a902127e
SrinathDsn <srinathsree1133@gmail.com>
2022-04-25 14:15:21 -0400

    Adding redis Setup

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -21,7 +21,7 @@ unzip /tmp/frontend.zip &>>$LOG_FILE && mv frontend-main/* . &>>$LOG_FILE && mv 
 StatCheck $?
 
 Print "Update roboshop Configruation"
-mv localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG_FILE && sed -i -e '/catalogue/s/localhost/catalogue.roboshop.internal/' /etc/nginx/default.d/roboshop.conf
+mv localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG_FILE && sed -i -e '/catalogue/s/localhost/catalogue.roboshop.internal/' -e '/user/s/localhost/user.roboshop.internal/' -e '/cart/s/localhost/cart.roboshop.internal/' /etc/nginx/default.d/roboshop.conf
 StatCheck $?
 
 Print "Starting Nginx"
