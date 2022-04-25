11f1f1db25adc54ca87f8bafee847679bdae7ba7
SrinathDsn <srinathsree1133@gmail.com>
2022-04-25 12:27:46 -0400

    Adding Catalogue Setup

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -21,7 +21,8 @@ unzip /tmp/frontend.zip &>>$LOG_FILE && mv frontend-main/* . &>>$LOG_FILE && mv 
 StatCheck $?
 
 Print "Update roboshop Configruation"
-mv localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG_FILE sed -i -e '/catalogue/s/localhost/catalogue.roboshop.internal/' /etc/nginx/default.d/roboshop.conf
+mv localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG_FILE
+sed -i -e '/catalogue/s/localhost/catalogue.roboshop.internal/' /etc/nginx/default.d/roboshop.conf
 StatCheck $?
 
 Print "Starting Nginx"
