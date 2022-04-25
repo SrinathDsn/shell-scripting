99513c70f2f0f2634dac7166fedde544407b3d52
SrinathDsn <srinathsree1133@gmail.com>
2022-04-24 21:53:18 -0400

    Functions

--- functions.sh
+++ functions.sh
@@ -16,11 +16,14 @@ Print_Message() {
   echo Good Morning,
   echo Welcome to ${1} Training
   echo "First Argument in Function = $1"
+  echo "value of a = $a"
   }
 
+a=10
 Print_Message abc
 
 echo "First Argument in Main Script = $1"
 
 ## Note, function has always been declared first and then you call function later in the code
+# Function weill have its own set of special variables
 
