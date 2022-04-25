7039cae879159833a228425d901f8605c00aa426
SrinathDsn <srinathsree1133@gmail.com>
2022-04-25 15:40:02 -0400

    mysql script

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -18,7 +18,7 @@ StatCheck $?
 echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('RoboShop@1');" >/tmp/rootpass.sql
 
 DEFAULT_ROOT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
-mysql -uroot -p"${DEFAULT_ROOT_PASSWORD}" </tmp/rootpass.sql &>>${LOG_FILE}
+mysql --connect-expired-password -uroot -p"${DEFAULT_ROOT_PASSWORD}" </tmp/rootpass.sql &>>${LOG_FILE}
   StatCheck $?
 
 
