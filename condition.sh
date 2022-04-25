a7bcd765cf0000ff608904208f3f36775b8ec07e
SrinathDsn <srinathsree1133@gmail.com>
2022-04-24 20:36:18 -0400

    Making frontend components

---
+++ condition.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# case and if are conditional commands, if command is widely because it has more options than case command.
+
+# If condition.
+# If found in three forms.
+
+# Simple If
+#if [ expression [
+# then
+# commands
+ # fi
+ #
+if [ 1 -eq 1]
+then
+  echo Hello
+fi
