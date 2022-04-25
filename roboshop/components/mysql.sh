55ed7174e2d2d8cd2338ec407a1d57e6f413ae41
SrinathDsn <srinathsree1133@gmail.com>
2022-04-25 15:06:17 -0400

    mysql script

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -15,9 +15,13 @@ Print "Start MySQL Service"
 systemctl enable mysqld &>>${LOG_FILE} && systemctl start mysqld &>>${LOG_FILE}
 StatCheck $?
 
-echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('Roboshop@1');" >/tmp/rootpass.sql
-
-DEFAULT_ROOT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
-mysql --connect-expired-password -uroot -p"${DEFAULT_ROOT_PASSWORD}" </tmp/rootpass.sql
+echo 'show databases' | mysql -uroot -pRoboshop@1 &>>${LOG_FILE}
+if [ $? -ne 0 ]; then
+  print "Change Default root password"
+  echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('Roboshop@1');" >/tmp/rootpass.sql
+  DEFAULT_ROOT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
+  mysql --connect-expired-password -uroot -p"${DEFAULT_ROOT_PASSWORD}" </tmp/rootpass.sql &>>${LOG_FILE}
+  StatCheck $?
+fi
 
 
