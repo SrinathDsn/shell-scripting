f96b72a238f12cf6f7ff58aa13b6ceaf29314af7
SrinathDsn <srinathsree1133@gmail.com>
2022-04-24 21:45:17 -0400

    Functions

--- functions.sh
+++ functions.sh
@@ -15,9 +15,12 @@ Print_Message() {
   echo Hello,
   echo Good Morning,
   echo Welcome to ${1} Training
+  echo "First Argument in Function = $1"
   }
 
 Print_Message DevOps
+
+echo "First Argument in Main Script = $1"
 
 ## Note, function has always been declared first and then you call function later in the code
 
