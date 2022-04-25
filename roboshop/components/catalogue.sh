a6b9de143aa2011b6168438ed3aaabe409f9e054
SrinathDsn <srinathsree1133@gmail.com>
2022-04-25 11:49:54 -0400

    Adding Catalogue Setup

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -3,26 +3,28 @@
 source components/common.sh
 
 Print "Configure YUM Repos"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash - &>>${LOG_FILE}
+curl -fsSL https://rpm.nodesource.com/setup_lts.x | bash - &>>${LOG_FILE}
 StatCheck $?
 
 Print "Install NodeJS"
 yum install nodejs gcc-c++ -y &>>${LOG_FILE}
 StatCheck $?
 
-Print "Add Application user"
 id ${APP_USER} &>>${LOG_FILE}
 if [ $? -ne 0 ]; then
+  Print "Add Application user"
   useradd ${APP_USER} &>>${LOG_FILE}
   StatCheck $?
 fi
 
 
 Print "Download App Component"
-curl curl -f -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>${LOG_FILE}
+curl -f -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>${LOG_FILE}
 StatCheck $?
 
-
+Print "Cleanup old content"
+rm -rf /home/${APP_USER}/catalogue &>>${LOG_FILE}
+StatCheck $?
 
 Print "Extract App Content"
 cd /home/${APP_USER} &>>${LOG_FILE} && unzip -o /tmp/catalogue.zip &>>${LOG_FILE} && mv catalogue-main catalogue &>>${LOG_FILE}
