5b9fabc1b3d0e797df8043dcd9ed9a2a87b75371
SrinathDsn <srinathsree1133@gmail.com>
2022-04-25 14:25:52 -0400

    loops

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -21,8 +21,13 @@ unzip /tmp/frontend.zip &>>$LOG_FILE && mv frontend-main/* . &>>$LOG_FILE && mv 
 StatCheck $?
 
 Print "Update roboshop Configruation"
-mv localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG_FILE && sed -i -e '/catalogue/s/localhost/catalogue.roboshop.internal/' -e '/user/s/localhost/user.roboshop.internal/' -e '/cart/s/localhost/cart.roboshop.internal/' /etc/nginx/default.d/roboshop.conf
-StatCheck $?
+mv localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG_FILE
+for component in catalogue user cart ; do
+  echo -e "Updating $component Configuration"
+  sed -i -e "/${component}/s/localhost/${component}.roboshop.internal/" /etc/nginx/default.d/roboshop.conf
+  StatCheck $?
+done
+
 
 Print "Starting Nginx"
 systemctl restart nginx &>>$LOG_FILE && systemctl enable nginx &>>$LOG_FILE
